6781ab6296e3b96bd1240447747de026afad760f
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2024-09-11 16:14:00 -0300

    Preparando Modificacion de UserPage

---
+++ client/src/modules/user/PageUser/UserData.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const UserData = () => {
+  /*
+  const userSchema = new Schema({
+  // basic properties
+  given_name:  { type: String,   required: true, maxLength: 50 },
+  family_name: { type: String,   required: true, maxLength: 50 },
+  full_name:   { type: String,   },
+  ! username:    { type: String,   unique: true  },
+  ! password:    { type: String,   required: true },
+  birthday:            Date,
+  ! public:      { type: Boolean,   default: true},
+
+  * document:    { type: String,   maxLength: 15 },
+  * documenttype:{ type: String,   enum: DOCTYPE },
+  
+  
+  ? email:       { type: String,   required: true, match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Debe completar un email valido'], unique: true },
+  ? phone:       { type: String, maxLength: 20   },
+  ? linkedinId:          String,
+  ? linkedinVerified:    Boolean,
+
+  photo:               String,
+  bio:                 String,
+
+   */
+  return (
+    <div>UserData</div>
+  )
+}
+
+export default UserData
