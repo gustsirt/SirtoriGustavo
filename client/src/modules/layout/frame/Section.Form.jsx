6690165f6b61c370802241387fb565b9753e7a3a
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2024-09-19 23:51:25 -0300

    Mejorado SecrionWForm

--- client/src/modules/layout/frame/Section.Form.jsx
+++ client/src/modules/layout/frame/Section.Form.jsx
@@ -1,46 +1,47 @@
 import React, { useState } from 'react'
-import { BiCheck, BiEditAlt, BiX } from 'react-icons/bi';
+import { BiEditAlt } from 'react-icons/bi';
+import { z } from 'zod';  
 import Modal from './Modal';
 import { useForm } from '@tanstack/react-form';
+import { zodValidator } from '@tanstack/zod-form-adapter';
 
-const SectionWForm = ({ title, css, data, setData, isEditable = true }) => {
+const SectionWForm = ({ title, css, data, setData }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  // tanstack Form
+  // Esquema de validación Zod
+  const schema = z.object({
+    name: z.string().min(3, "El nombre debe tener al menos 3 caracteres"),
+    email: z.string().email("Debe ser un email válido")
+  });
+
+  // Configuración de Tanstack Form
   const form = useForm({
     defaultValues: data,
+    validatorAdapter: zodValidator(schema),
+    validators: {
+      onChange: schema
+    },
     onSubmit: ({value}) => {
       console.log(value)
-      //setData(value);
+      setData(value);
       handleCloseModal();
     }
   })
 
-  const handleEditClick = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
+  const handleEditClick  = () => { setIsModalOpen(true);  };
+  const handleCloseModal = () => { setIsModalOpen(false); };
 
   return (
     <>
-      {/* ! Sección principal con datos */}
+      {/* Sección principal con datos */}
       <div className={`mb-8 p-6 border rounded-lg bg-gray-50 ${css || ''}`}>
-        {(title || isEditable) && 
-          <div className="flex justify-between items-center mb-4">
-            <h3 className="text-lg font-semibold mb-4">{title || ""}</h3>
-            {isEditable && (
-              <button
-                onClick={handleEditClick}
-                className="text-blue-500 hover:underline"
-              >
-                <BiEditAlt />
-              </button>
-            )}
-          </div>
-        }
+        <div className="flex justify-between items-center mb-4">
+          <h3 className="text-lg font-semibold mb-4">{title || ""}</h3>
+          <button onClick={handleEditClick} className="text-blue-500 hover:underline">
+            <BiEditAlt />
+          </button>
+        </div>
+
         {/* Aquí mostramos los datos pasados por props */}
         <div>
           <p><strong>Nombre:</strong> {data.name}</p>
@@ -48,70 +49,81 @@ const SectionWForm = ({ title, css, data, setData, isEditable = true }) => {
         </div>
       </div>
 
-      {/* ! Modal con formulario */}
+      {/* Modal con formulario */}
       <Modal isOpen={isModalOpen} onClose={handleCloseModal} title={`Editar ${title}`}>
       <form onSubmit={(e) => {
-          e.preventDefault();
-          e.stopPropagation();
-        }}>
-          {/* Inputs del formulario, reutilizando Tanstack Form */}
-          <form.Field name="name"  
-            validators={{
-              onChangeAsyncDebounceMs: 500,
-              onChangeAsync: ({value}) => {if (value.length < 3 ) { return "debe tener mas de 3 caracteres"}}
-            }}
-            children={(field) => {
-              return (
-                <>
-                  <label htmlFor={field.name} className="block mb-2">Nombre:</label>
-                  <input
-                    id={field.name}
-                    name={field.name}
-                    type='text'
-                    value={field.state.value}
-                    className="w-full border border-gray-300 p-2 rounded mb-4"
-                    onChange={(e) => field.handleChange(e.target.value)}
-                  />
-                  {field.state.meta.errors && (
-                    <p className="text-red-500">{field.state.meta.errors}</p>
-                  )}
-                </>
-              )
-            }}
+        e.preventDefault();
+        // e.stopPropagation();
+        form.handleSubmit();
+      }}>
+          {/* Inputs del formulario, Tanstack Form */}
+          <form.Field name="name"
+            children={(field) => (
+              <div className="my-3">
+                <label htmlFor={field.name} className="block mb-2">
+                  Nombre:
+                </label>
+                <input
+                  id={field.name}
+                  name={field.name}
+                  type="text"
+                  value={field.state.value}
+                  className={`w-full border p-2 rounded mb-1 ${field.state.meta.errors.length > 0 ? 'border-red-500' : 'border-gray-300'}`}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                />
+              </div>
+            )}
           />
-          <form.Field name="email" 
-            children={(field) => {
-              return (
-                <>
-                  <label htmlFor={field.name} className="block mb-2">Email:</label>
-                  <input
-                    id={field.name}
-                    name={field.name}
-                    type='email'
-                    value={field.state.value}
-                    className="w-full border border-gray-300 p-2 rounded mb-4"
-                    onChange={(e) => field.handleChange(e.target.value)}
-                  />
-                </>
-              )
-            }}
+          <form.Field name="email"
+            children={(field) => (
+              <div className="my-3">
+                <label htmlFor={field.name} className="block mb-2">Email:</label>
+                <input
+                  id={field.name}
+                  name={field.name}
+                  type="email"
+                  value={field.state.value}
+                  className={`w-full border p-2 rounded mb-1 ${field.state.meta.errors.length > 0 ? 'border-red-500' : 'border-gray-300'}`}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                />
+              </div>
+            )}
           />
-          <div className="mt-4 flex justify-end">
-            <button
-              onClick={handleCloseModal}
-              className="bg-gray-500 text-white py-2 px-4 rounded mr-2"
-              type="button"
-            >
-              <BiX/>
-            </button>
-            <button
-              onClick={form.handleSubmit}
-              className="bg-blue-500 text-white py-2 px-4 rounded"
-              type="submit"
-            >
-              <BiCheck />
-            </button>
-          </div>
+          {/* Alertas Errores, Tanstack Form */}
+          <form.Subscribe
+            selector={(state) => state.errors}
+            children={(errors) =>
+              errors.length > 0 && (
+                <p className="text-red-500 text-sm my-3">{errors}</p>
+              )
+            }
+          />
+          {/* Botones de acción, Tanstack Form */}
+          <form.Subscribe
+            selector={(state) => [state.canSubmit]}
+            children={([canSubmit]) => (
+              <div className="flex items-center justify-between mt-6">
+                <button
+                  type="submit"
+                  disabled={!canSubmit}
+                  className={`${
+                    !canSubmit
+                      ? 'bg-gray-400'
+                      : 'bg-indigo-600 hover:bg-indigo-700'
+                  } text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline`}
+                >
+                  Guardar
+                </button>
+                <button
+                  type="reset"
+                  onClick={() => form.reset()}
+                  className="text-indigo-600 hover:underline"
+                >
+                  Restablecer
+                </button>
+              </div>
+            )}
+          />
         </form>
       </Modal>
     </>
