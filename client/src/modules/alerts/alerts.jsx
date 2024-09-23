6b13f8dea5c6b422b0438c3b17cc2d666fc4dcfc
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2024-09-23 16:15:37 -0300

    Front More Alerts

--- client/src/modules/alerts/alerts.jsx
+++ client/src/modules/alerts/alerts.jsx
@@ -1,21 +1,47 @@
 import Swal from 'sweetalert2'
 
-const toast = Swal.mixin({
-  toast: true,
-  position: "top-end",
-  showConfirmButton: false,
-  // timer: 3000,
-  timerProgressBar: true,
-  didOpen: (toast) => {
-      toast.onmouseenter = Swal.stopTimer;
-      toast.onmouseleave = Swal.resumeTimer;
-  }
-});
+export const alertBasic = (title, text, icon) => {
+  Swal.fire({
+    title,
+    text,
+    icon
+  });
+}
 
 export const alertMessage = (title, icon="", time=3) => {
-  toast.fire({
+  Swal.fire({
     icon,
     title,
     timer: time*1000,
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+    },
   })
 }
+
+export const alertAreYouSure = (action) => {
+  Swal.fire({
+    title: "¿Estas seguro?",
+    text: "Este cambio es irreversible",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Si. Hazlo",
+    cancelButtonText: "Lo pensare",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      action && action()
+      Swal.fire({
+        title: "¡Eliminado!",
+        icon: "success"
+      });
+    }
+  });
+}
+
