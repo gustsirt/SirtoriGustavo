a18cb46e6e8bf1d1e75c5cfbd1f9bb6aeb01b8cc
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2024-09-30 16:06:52 -0300

    Front Actualizado gestion de autorización

--- client/src/store/UserSlice.jsx
+++ client/src/store/UserSlice.jsx
@@ -1,16 +1,45 @@
 // * Store de Usuarios
-
 import axiosInstance from "../config/axiosInstance";
+import { jwtDecode  } from 'jwt-decode';
 
 const createUserSlice = (set, get) => ({
-  isAuthenticated: !!localStorage.getItem('token'),
+  isAuthenticated: false,
+  checkAuth: () => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      try {
+        const decodedToken = jwtDecode(token); // Decodifica el token
+        const currentTime = Date.now() / 1000; // Obtiene el tiempo actual en segundos
+    
+        // Verifica si el token ha expirado
+        if (decodedToken.exp && decodedToken.exp > currentTime) {
+          set({ isAuthenticated: true });
+          return true;
+        } else {
+          set({ isAuthenticated: false });
+          return false;
+        }
+      } catch (error) {
+        console.error('Error decoding token:', error);
+        set({ isAuthenticated: false });
+        return false;
+      }
+    } else {
+      set({ isAuthenticated: false });
+      return false;
+    }
+  },
+
+  // Obtiene el token desde localStorage
   getToken: () => localStorage.getItem('token') || null,
 
+  // Almacena el token y marca como autenticado
   login: (token) => {
     localStorage.setItem('token', token);
     set({ token, isAuthenticated: true });
   },
 
+  // Elimina el token y desautentica al usuario
   logout: () => {
     localStorage.removeItem('token');
     set({ currentUser: null, isAuthenticated: false });
@@ -19,18 +48,24 @@ const createUserSlice = (set, get) => ({
   currentUser: null,
   currentUserName:null,
 
+  // Establece el usuario actual
   setUser: (user) => set({ currentUser: user }),
 
+  // Obtiene el usuario actual de la API
   getUser: async () => {
-    const user = await axiosInstance("/v1/users/current")
-    
-    if (!user.data.isError) {
-      set({
-        currentUser: user.data.data,
-        currentUserName: user.data.data.username,
-      })
-    } else {
-      console.error('Error fetching user data:', user.data.message);
+    try {
+      const user = await axiosInstance("/v1/users/current");
+
+      if (user.status === 200 && !user.data.isError) {
+        set({
+          currentUser: user.data.data,
+          currentUserName: user.data.data.username,
+        });
+      } else {
+        console.error('Error fetching user data:', user.data.message || 'Unknown error');
+      }
+    } catch (error) {
+      console.error('Error during API call:', error.message || error);
     }
   }
 })
