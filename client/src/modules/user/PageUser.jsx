976b5da901f19870f4fdd50a7e0b99aaaad14f33
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2024-10-01 00:11:23 -0300

    Front SectionForm + Aplicaciones

--- client/src/modules/user/PageUser.jsx
+++ client/src/modules/user/PageUser.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BiEditAlt } from 'react-icons/bi'
 import { useAppStore } from '../../store/useAppStore';
+import { updateCurrentUser, userUpdatePhoto } from '../../apis/users.services'
 import UserData from './PageUser/UserData';
 import Header from './PageUser/Header';
 import Section from '../layout/frame/Section';
@@ -14,9 +15,18 @@ const PageUser = ({user}) => {
   const { currentUserName } = useAppStore()
   const itsMyProfile = user.username === currentUserName
 
-  // console.log("user: ",user);
+  console.log("user: ",user);
   // console.log("itsMyProfile: ", itsMyProfile);
-  
+
+  function handleAction(data) {
+    updateCurrentUser(data)
+    window.location.reload();
+  }
+  function handleActionPhoto(photo) {
+    userUpdatePhoto(photo)
+    window.location.reload();
+  }
+
   return (
     <>
       <Section>
@@ -26,8 +36,8 @@ const PageUser = ({user}) => {
       </Section>
 
       {/* Sección con información del usuario */}
-      <UserData user={user} itsMyProfile={itsMyProfile} />
-      <UserBio user={user} itsMyProfile={itsMyProfile} />
+      <UserData user={user} itsMyProfile={itsMyProfile} action={handleAction} />
+      <UserBio user={user} itsMyProfile={itsMyProfile} action={handleAction} />
 
       <Proyects/>
       <Experience/>
--- client/src/modules/user/PageUser/UserBio.jsx
+++ client/src/modules/user/PageUser/UserBio.jsx
@@ -2,12 +2,16 @@ import React, { useState } from 'react'
 import { BiSolidUserDetail } from 'react-icons/bi'
 import SectionWForm from '../../layout/frame/Section.Form';
 
-const UserBio = ({user, itsMyProfile }) => {
+const UserBio = ({user, itsMyProfile, action }) => {
   // Datos a pasar
   const [data, setData] = useState({
     bio: user.bio,          // Biografia del usuario
   });
 
+  function handleAction(data) {
+    setData(data)
+    action(data)
+  }
   // Definición de los campos con validación
   const fields = [
     { name: "bio", label: "Descripción", icon:BiSolidUserDetail, type: "textarea" },
@@ -17,7 +21,7 @@ const UserBio = ({user, itsMyProfile }) => {
     <SectionWForm
       title="Descripción"
       data={data}
-      setData={setData}
+      setData={handleAction}
       isEditable={true}
       isPublic={true}
       fields={fields}
